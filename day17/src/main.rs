955319b61c68e318d5e91296031eef96870b214c
Edvard Pettersen <edvard.pettersen@elas.ai>
2023-12-01 10:22:19 +0100

    More boilerplate

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -1,3 +1,31 @@
+use std::io::Read;
+
+#[derive(Debug)]
+struct Input {
+    lines: Vec<Vec<u8>>,
+}
+
+impl Input {
+    fn parse() -> Result<Self, Box<dyn std::error::Error>> {
+        let stdin = std::io::stdin();
+        let mut input = String::new();
+        stdin.lock().read_to_string(&mut input)?;
+        let lines = input.lines().map(|line| line.as_bytes().to_vec()).collect();
+
+        Ok(Self { lines })
+    }
+}
+
+fn part1(input: &Input) -> usize {
+    unimplemented!("part1")
+}
+
+fn part2(input: &Input) -> usize {
+    unimplemented!("part2")
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = Input::parse().unwrap();
+    println!("part1: {}", part1(&input));
+    println!("part2: {}", part2(&input));
 }
