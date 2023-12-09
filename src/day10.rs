8db799121acea5749e29bce166c491c5a3b22954
Edvard Pettersen <edvard.pettersen@gmail.com>
2023-12-09 21:50:58 +0100

    Re-impl Day 10 based on reddit inspiration

--- src/day10.rs
+++ src/day10.rs
@@ -1,24 +1,31 @@
-use std::collections::{HashSet, VecDeque};
+#![allow(clippy::reversed_empty_ranges)] 
+
+use std::collections::HashSet;
+use ndarray::{s, Array2};
 
 #[derive(Debug)]
 pub struct Input {
-    map: Vec<Vec<u8>>,
+    map: Map,
 }
 
 impl From<&str> for Input {
     fn from(s: &str) -> Self {
-        let mut map: Vec<Vec<u8>> = s
+        let width: usize = s.lines().next().unwrap().len();
+        let inner_map: Vec<char> = s
             .lines()
-            .map(|line| {
-                std::iter::once(b'.')
-                    .chain(line.as_bytes().iter().copied())
-                    .chain(std::iter::once(b'.'))
-                    .collect()
+            .flat_map(|line| line.chars())
+            .map(|c| match c {
+                'F' => '┌',
+                'J' => '┘',
+                'L' => '└',
+                '7' => '┐',
+                c => c,
             })
             .collect();
-        let len = map[0].len();
-        map.insert(0, vec![b'.'; len]);
-        map.push(vec![b'.'; len]);
+        let height = inner_map.len() / width;
+        let inner_map = Array2::from_shape_vec((height, width), inner_map).unwrap();
+        let mut map = Array2::from_elem((height + 2, width + 2), '.');
+        map.slice_mut(s![1..-1, 1..-1]).assign(&inner_map);
         Self { map }
     }
 }
@@ -31,187 +38,110 @@ enum Direction {
     Right,
 }
 
-type Coord = (usize, usize);
-
-fn neighbors(coord: &Coord) -> [Coord; 4] {
-    [
-        (coord.0 - 1, coord.1),
-        (coord.0 + 1, coord.1),
-        (coord.0, coord.1 - 1),
-        (coord.0, coord.1 + 1),
-    ]
-}
-
-fn traverse(
-    map: &[Vec<u8>],
-    mut curr: Coord,
-    mut direction: Direction,
-) -> Option<(Vec<(Coord, Direction)>, bool)> {
-    let mut path = Vec::new();
-    let mut rotation: isize = 0;
-    loop {
-        path.push((curr, direction));
-        let tile = map[curr.0][curr.1];
-        curr = match (tile, direction) {
-            (b'S' | b'J' | b'L' | b'|', Direction::Up) => (curr.0 - 1, curr.1),
-            (b'S' | b'F' | b'7' | b'|', Direction::Down) => (curr.0 + 1, curr.1),
-            (b'S' | b'J' | b'7' | b'-', Direction::Left) => (curr.0, curr.1 - 1),
-            (b'S' | b'L' | b'F' | b'-', Direction::Right) => (curr.0, curr.1 + 1),
-            _ => return None,
-        };
-
-        let tile = map[curr.0][curr.1];
-        direction = match (tile, direction) {
-            (b'.', _) => return None,
-            (b'S', _) => return Some((path, rotation > 0)),
-            (b'|', _) => direction,
-            (b'-', _) => direction,
-            (b'J', Direction::Right) => {
-                rotation -= 1;
-                Direction::Up
-            }
-            (b'J', Direction::Down) => {
-                rotation += 1;
-                Direction::Left
-            }
-            (b'L', Direction::Down) => {
-                rotation -= 1;
-                Direction::Right
-            }
-            (b'L', Direction::Left) => {
-                rotation += 1;
-                Direction::Up
-            }
-            (b'F', Direction::Left) => {
-                rotation -= 1;
-                Direction::Down
-            }
-            (b'F', Direction::Up) => {
-                rotation += 1;
-                Direction::Right
-            }
-            (b'7', Direction::Up) => {
-                rotation -= 1;
-                Direction::Left
-            }
-            (b'7', Direction::Right) => {
-                rotation += 1;
-                Direction::Down
-            }
-            _ => return None,
-        }
+impl Direction {
+    fn iter() -> impl Iterator<Item = Direction> {
+        [
+            Direction::Up,
+            Direction::Down,
+            Direction::Left,
+            Direction::Right,
+        ]
+        .into_iter()
     }
 }
 
-fn explore(map: &[Vec<u8>], start: Coord) -> (Vec<(Coord, Direction)>, bool) {
-    [
-        Direction::Up,
-        Direction::Down,
-        Direction::Left,
-        Direction::Right,
-    ]
-    .iter()
-    .filter_map(|dir| traverse(map, start, *dir))
-    .next()
-    .unwrap()
+type Map = Array2<char>;
+type Coord = (usize, usize);
+
+fn traverse(map: &Map, mut curr: Coord, mut direction: Direction) -> Option<(Vec<Coord>, char)> {
+    let first_direction = direction;
+    let mut path = Vec::new();
+    'outer: loop {
+        path.push(curr);
+
+        let tile = map[curr];
+        curr = match (tile, direction) {
+            ('S' | '┘' | '└' | '|', Direction::Up) => (curr.0 - 1, curr.1),
+            ('S' | '┌' | '┐' | '|', Direction::Down) => (curr.0 + 1, curr.1),
+            ('S' | '┘' | '┐' | '-', Direction::Left) => (curr.0, curr.1 - 1),
+            ('S' | '└' | '┌' | '-', Direction::Right) => (curr.0, curr.1 + 1),
+            _ => return None,
+        };
+
+        let tile = map[curr];
+        direction = match (tile, direction) {
+            ('.', _) => return None,
+            ('S', _) => break 'outer,
+            ('|', _) => direction,
+            ('-', _) => direction,
+            ('┘', Direction::Right) => Direction::Up,
+            ('┘', Direction::Down) => Direction::Left,
+            ('└', Direction::Down) => Direction::Right,
+            ('└', Direction::Left) => Direction::Up,
+            ('┌', Direction::Left) => Direction::Down,
+            ('┌', Direction::Up) => Direction::Right,
+            ('┐', Direction::Up) => Direction::Left,
+            ('┐', Direction::Right) => Direction::Down,
+            combo => unreachable!("invalid combination: {:?}", combo),
+        }
+    }
+
+    let piece = match (direction, first_direction) {
+        (Direction::Up, Direction::Up) | (Direction::Down, Direction::Down) => '|',
+        (Direction::Left, Direction::Left) | (Direction::Right, Direction::Right) => '-',
+        (Direction::Right, Direction::Up) | (Direction::Down, Direction::Left) => '┘',
+        (Direction::Up, Direction::Right) | (Direction::Left, Direction::Down) => '┌',
+        (Direction::Down, Direction::Right) | (Direction::Left, Direction::Up) => '└',
+        (Direction::Up, Direction::Left) | (Direction::Right, Direction::Down) => '┐',
+        combo => unreachable!("invalid combination: {:?}", combo),
+    };
+
+    Some((path, piece))
+}
+
+fn explore(map: &Map, start: Coord) -> (Vec<Coord>, char) {
+    Direction::iter()
+        .filter_map(|dir| traverse(map, start, dir))
+        .next()
+        .unwrap()
 }
 
 pub fn part1(input: &Input) -> usize {
     let start = input
         .map
-        .iter()
-        .enumerate()
-        .flat_map(|(r, row)| row.iter().enumerate().map(move |(c, v)| ((r, c), v)))
-        .find_map(|(coord, v)| if *v == b'S' { Some(coord) } else { None })
+        .indexed_iter()
+        .find_map(|(coord, c)| if *c == 'S' { Some(coord) } else { None })
         .unwrap();
 
-    let (path, _rotation) = explore(&input.map, start);
+    let (path, _) = explore(&input.map, start);
     path.len() / 2
 }
 
 pub fn part2(input: &Input) -> usize {
     let start = input
         .map
-        .iter()
-        .enumerate()
-        .flat_map(|(r, row)| row.iter().enumerate().map(move |(c, v)| ((r, c), v)))
-        .find_map(|(coord, v)| if *v == b'S' { Some(coord) } else { None })
+        .indexed_iter()
+        .find_map(|(coord, c)| if *c == 'S' { Some(coord) } else { None })
         .unwrap();
 
-    let (path, rotation) = explore(&input.map, start);
-    let on_path: HashSet<Coord> = path.iter().map(|(coord, _)| *coord).collect();
-    let adjacent: HashSet<Coord> = path
-        .iter()
-        .flat_map(|(coord, direction)| {
-            match (direction, rotation) {
-                (Direction::Up, false) => [
-                    (coord.0, coord.1 - 1),
-                    (coord.0 - 1, coord.1 - 1),
-                    (coord.0 - 1, coord.1),
-                ],
-                (Direction::Up, true) => [
-                    (coord.0, coord.1 + 1),
-                    (coord.0 - 1, coord.1 + 1),
-                    (coord.0 - 1, coord.1),
-                ],
-                (Direction::Down, false) => [
-                    (coord.0, coord.1 + 1),
-                    (coord.0 + 1, coord.1 + 1),
-                    (coord.0 + 1, coord.1),
-                ],
-                (Direction::Down, true) => [
-                    (coord.0, coord.1 - 1),
-                    (coord.0 + 1, coord.1 - 1),
-                    (coord.0 + 1, coord.1),
-                ],
-                (Direction::Left, false) => [
-                    (coord.0 + 1, coord.1),
-                    (coord.0 + 1, coord.1 - 1),
-                    (coord.0, coord.1 - 1),
-                ],
-                (Direction::Left, true) => [
-                    (coord.0 - 1, coord.1),
-                    (coord.0 - 1, coord.1 - 1),
-                    (coord.0, coord.1 - 1),
-                ],
-                (Direction::Right, false) => [
-                    (coord.0 - 1, coord.1),
-                    (coord.0 - 1, coord.1 + 1),
-                    (coord.0, coord.1 + 1),
-                ],
-                (Direction::Right, true) => [
-                    (coord.0 + 1, coord.1),
-                    (coord.0 + 1, coord.1 + 1),
-                    (coord.0, coord.1 + 1),
-                ],
-            }
-            .into_iter()
-        })
-        .filter(|coord| !on_path.contains(coord))
-        .collect();
+    let (path, start_tile) = explore(&input.map, start);
+    let path: HashSet<Coord> = path.into_iter().collect();
 
-    let mut flooded: HashSet<Coord> = HashSet::new();
-    let mut nest: HashSet<Coord> = HashSet::new();
+    let mut map = input.map.clone();
+    map[start] = start_tile;
 
-    for curr in adjacent {
-        if flooded.contains(&curr) {
-            continue;
-        }
-
-        let mut flood: HashSet<Coord> = HashSet::new();
-        let mut queue = VecDeque::from([curr]);
-        while let Some(tile) = queue.pop_front() {
-            if on_path.contains(&tile) {
-                continue;
-            }
-            if flood.insert(tile) {
-                queue.extend(neighbors(&tile).into_iter());
+    let mut total_inside = 0;
+    for (r, row) in map.rows().into_iter().enumerate() {
+        let mut inside = false;
+        for (c, tile) in row.iter().enumerate() {
+            let coord = (r, c);
+            match *tile {
+                '|' | '┌' | '┐' if path.contains(&coord) => inside = !inside,
+                _ if inside && !path.contains(&coord) => total_inside += 1,
+                _ => {}
             }
         }
-
-        flooded.extend(flood.iter());
-        nest.extend(flood.iter());
     }
 
-    nest.len()
+    total_inside
 }
